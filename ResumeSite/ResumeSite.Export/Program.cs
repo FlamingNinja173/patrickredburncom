da82fe2ac14a35cf103087f6c71858a7898eba79
Patrick <pdredburn@gmail.com>
2025-07-08 18:49:47 -0500

    Add new projects and restructure solution files.

---
+++ ResumeSite/ResumeSite.Export/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ResumeSite.Export
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
