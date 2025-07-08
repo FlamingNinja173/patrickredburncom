571274a8c13a0ad4008b75ac931a89d26c14a956
Patrick <pdredburn@gmail.com>
2025-07-08 18:22:03 -0500

    Add Footer and dark mode support

--- ResumeSite/ResumeSite/Constants/Constants.Configuration.cs
+++ ResumeSite/ResumeSite/Constants/Constants.Configuration.cs
@@ -5,6 +5,8 @@
         public const string AppName = "Patrick Redburn";
         public const string AppVersion = "1.0.0";
         public const string AppDescription = "A personal resume site showcasing Patrick Redburn's skills, certifications, and experience.";
+        public const string AppAuthor = "Patrick Redburn";
+        public const string AppYear = "2025";
         public const string ContactEmail = "pdredburn@gmail.com";
     }
 }
