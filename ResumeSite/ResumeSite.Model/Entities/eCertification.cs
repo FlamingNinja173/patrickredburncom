0555e1b9093c17bc16818af06d4f65af651a3d81
Patrick <pdredburn@gmail.com>
2025-07-12 11:02:13 -0500

    Add CredentialUrl to eCertification and update JSON

--- ResumeSite/ResumeSite.Model/Entities/eCertification.cs
+++ ResumeSite/ResumeSite.Model/Entities/eCertification.cs
@@ -7,5 +7,6 @@
         public DateOnly DateEarned { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; } = string.Empty;
+        public string? CredentialUrl { get; set; } = string.Empty;
     }
 }
