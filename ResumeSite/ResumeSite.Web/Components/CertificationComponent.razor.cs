edc7eb36a091c9fb4c193b19a7782de3d311bd71
Patrick <pdredburn@gmail.com>
2025-07-12 11:03:06 -0500

    Add CertificationComponent for displaying certifications

---
+++ ResumeSite/ResumeSite.Web/Components/CertificationComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using ResumeSite.Models;
+using ResumeSite.Web.Services;
+
+namespace ResumeSite.Web.Components
+{
+    public class CertificationComponentCode : ComponentBase
+    {
+        [Inject]
+        public ICertificationService Service { get; set; } = default!;
+
+        protected List<eCertification> certifications = [];
+
+        protected override async Task OnInitializedAsync()
+        {
+            certifications = await Service.GetCertifications();
+        }
+    }
+}
