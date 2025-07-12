4303002f4e7bc5ef6e6c6a3343751a0b9e27955b
Patrick <pdredburn@gmail.com>
2025-07-12 12:24:12 -0500

    Refactor test classes and add new service tests

--- ResumeSite/ResumeSite.Tests/Web/EducationServiceTests.cs
+++ ResumeSite/ResumeSite.Tests/Web/EducationServiceTests.cs
@@ -2,23 +2,23 @@
 using System.Net;
 using System.Text;
 
-namespace ResumeSite.Tests
+namespace ResumeSite.Tests.Web
 {
-    public class CertificationServiceTests
+    public class EducationServiceTests
     {
         [Fact]
-        public async Task GetCertifications_ReturnsData()
+        public async Task GetEducation_ReturnsData()
         {
-            var json = "[{\"Name\":\"Test\",\"Issuer\":\"Issuer\",\"DateEarned\":\"2024-01-01\",\"ImageUrl\":\"url\",\"CredentialUrl\":\"cred\"}]";
+            var json = "[{\"School\":\"UCO\",\"Degree\":\"BS\",\"StartDate\":\"2018-01-01\",\"EndDate\":\"2020-01-01\"}]";
             var handler = new FakeHandler(json);
             var http = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
-            ICertificationService service = new CertificationService(http);
+            IEducationService service = new EducationService(http);
 
-            var result = await service.GetCertifications();
+            var result = await service.GetEducation();
 
             Assert.Single(result);
-            Assert.Equal("Test", result[0].Name);
-            Assert.Equal("cred", result[0].CredentialUrl);
+            Assert.Equal("UCO", result[0].School);
+            Assert.Equal("BS", result[0].Degree);
         }
 
         private class FakeHandler : HttpMessageHandler
---
+++ ResumeSite/ResumeSite.Tests/Web/SkillServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using ResumeSite.Web.Services;
+using System.Net;
+using System.Text;
+
+namespace ResumeSite.Tests.Web
+{
+    public class SkillServiceTests
+    {
+        [Fact]
+        public async Task GetSkills_ReturnsData()
+        {
+            var json = "[{\"Name\":\"C#\",\"Description\":\"Lang\",\"Proficiency\":4}]";
+            var handler = new FakeHandler(json);
+            var http = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+            ISkillService service = new SkillService(http);
+
+            var result = await service.GetSkills();
+
+            Assert.Single(result);
+            Assert.Equal("C#", result[0].Name);
+            Assert.Equal(Constants.SkillProficiency.Expert, result[0].Proficiency);
+        }
+
+        private class FakeHandler : HttpMessageHandler
+        {
+            private readonly string response;
+
+            public FakeHandler(string response)
+            {
+                this.response = response;
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var msg = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(response, Encoding.UTF8, "application/json")
+                };
+                return Task.FromResult(msg);
+            }
+        }
+    }
+}
