2ba28bdfd159622d4dbfcf946ec8daf38a24aafd
Patrick <pdredburn@gmail.com>
2025-07-12 11:02:24 -0500

    Add CertificationService and ICertificationService interface

---
+++ ResumeSite/ResumeSite.Web/Services/CertificationService.cs
@@ -0,0 +1,16 @@
+﻿using ResumeSite.Models;
+using System.Net.Http.Json;
+
+namespace ResumeSite.Web.Services
+{
+    public class CertificationService(HttpClient http) : ICertificationService
+    {
+        private readonly HttpClient http = http;
+
+        public async Task<List<eCertification>> GetCertifications()
+        {
+            var retVal = await http.GetFromJsonAsync<List<eCertification>>("sample-data/certifications.json");
+            return retVal ?? [];
+        }
+    }
+}
---
+++ ResumeSite/ResumeSite.Web/Services/ICertificationService.cs
@@ -0,0 +1,9 @@
+﻿using ResumeSite.Models;
+
+namespace ResumeSite.Web.Services
+{
+    public interface ICertificationService
+    {
+        Task<List<eCertification>> GetCertifications();
+    }
+}
